9f2cff77c7470cec55332362a950a37127070d09
akshayfanatic <akshay@fanaticcoders.com>
2026-08-20 15:48:29 +0530

    fix(typo): fix hook typo from dialog

--- src/components/dashboard/profile/ProfilePhotoUploader.tsx
+++ src/components/dashboard/profile/ProfilePhotoUploader.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { useOptionalDialog } from '@/components/shared/action-dialog';
+import { useDialog } from '@/components/shared/action-dialog';
 import { AvatarUploader } from '@/modules/uploads/components/AvatarUploader';
 import { useAuth } from '@/providers/AuthProvider';
 
 export function ProfilePhotoUploader() {
-  const dialog = useOptionalDialog();
+  const dialog = useDialog();
   const { session, refetch } = useAuth();
 
   return (
